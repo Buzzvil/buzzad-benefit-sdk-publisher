67c5670a31af0650436e6efedc01ab83517c4684
SeoJungHong <hsj0223@gmail.com>
2018-12-03 11:45:42 +0900

    1.0 API 에 맞게 샘플 수정

--- app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/MainActivity.java
+++ app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/MainActivity.java
@@ -15,10 +15,13 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.buzzvil.buzzad.benefit.core.ad.AdError;
 import com.buzzvil.buzzad.benefit.core.models.Ad;
-import com.buzzvil.buzzad.benefit.ui.feed.FeedHandler;
-import com.buzzvil.buzzad.benefit.ui.media.CtaView;
-import com.buzzvil.buzzad.benefit.ui.media.MediaView;
-import com.buzzvil.buzzad.benefit.ui.nativead.NativeAd;
+import com.buzzvil.buzzad.benefit.presentation.feed.FeedConfig;
+import com.buzzvil.buzzad.benefit.presentation.feed.FeedHandler;
+import com.buzzvil.buzzad.benefit.presentation.media.CtaView;
+import com.buzzvil.buzzad.benefit.presentation.media.MediaView;
+import com.buzzvil.buzzad.benefit.presentation.nativead.NativeAd;
+import com.buzzvil.buzzad.benefit.presentation.nativead.NativeAdLoader;
+import com.buzzvil.buzzad.benefit.presentation.nativead.NativeAdView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     private Button feedButton;
     private ProgressBar progressBar;
     private View interstitialView;
-    private NativeAd nativeAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +51,11 @@ public class MainActivity extends AppCompatActivity {
         feedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final FeedHandler feedHandler = new FeedHandler();
+                final FeedConfig feedConfig = new FeedConfig.Builder(App.UNIT_ID_FEED)
+                        .title("BuzzAdBenefit Feed")
+                        .primaryColor("#1290FF")
+                        .build();
+                final FeedHandler feedHandler = new FeedHandler(feedConfig);
                 feedHandler.startFeedActivity(MainActivity.this);
             }
         });
@@ -59,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadNativeAd() {
         progressBar.setVisibility(View.VISIBLE);
-        nativeAd = new NativeAd(this, App.UNIT_ID_NATIVE_AD);
-        nativeAd.setAdListener(new NativeAd.OnAdEventListener() {
+        final NativeAdLoader loader = new NativeAdLoader(this, App.UNIT_ID_NATIVE_AD);
+        loader.loadAd(new NativeAdLoader.OnAdLoadedListener() {
             @Override
             public void onLoadError(@NonNull AdError adError) {
                 progressBar.setVisibility(View.GONE);
@@ -68,9 +74,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onAdLoaded(@NonNull Ad ad) {
+            public void onAdLoaded(@NonNull NativeAd nativeAd) {
                 progressBar.setVisibility(View.GONE);
-                interstitialView = populateAd(ad);
+
+                nativeAd.setAdEventListener(new NativeAd.OnAdEventListener() {
+                    @Override
+                    public void onAdParticipated(@NonNull NativeAd nativeAd) {
+                        final CtaView ctaView = interstitialView.findViewById(R.id.ad_cta_view);
+                        ctaView.setParticipated(true);
+                        ctaView.setRewardText(null);
+                    }
+                });
+
+                interstitialView = populateAd(nativeAd);
                 interstitialView.findViewById(R.id.ad_close_text).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -79,26 +95,14 @@ public class MainActivity extends AppCompatActivity {
                 });
                 ((ViewGroup) findViewById(android.R.id.content)).addView(interstitialView);
             }
-
-            @Override
-            public void onAdParticipated(@NonNull Ad ad) {
-                final CtaView ctaView = interstitialView.findViewById(R.id.ad_cta_view);
-                ctaView.setParticipated(true);
-                ctaView.setRewardText(null);
-            }
-
-            @Override
-            public void onAdClicked(@NonNull Ad ad) {
-            }
         });
-        nativeAd.loadAd();
     }
 
-    private View populateAd(final Ad ad) {
-        nativeAd.unregisterView();
+    private View populateAd(final NativeAd nativeAd) {
+        final Ad ad = nativeAd.getAd();
 
         final View interstitialView = LayoutInflater.from(this).inflate(R.layout.view_interstitial_ad, null, false);
-        final View adView = interstitialView.findViewById(R.id.ad_view);
+        final NativeAdView nativeAdView = interstitialView.findViewById(R.id.native_ad_view);
         final MediaView mediaView = interstitialView.findViewById(R.id.ad_media_view);
         final TextView titleTextView = interstitialView.findViewById(R.id.ad_title_text);
         final TextView descriptionTextView = interstitialView.findViewById(R.id.ad_description_text);
@@ -113,25 +117,24 @@ public class MainActivity extends AppCompatActivity {
         ctaView.setRewardText(String.format(Locale.US, "+%d", ad.getReward()));
         ctaView.setCallToActionText(ad.getCallToAction());
 
-        // 3) Create a list of clickable views and register it.
+        // 3) Create a list of clickable views.
         final List<View> clickableViews = new ArrayList<>();
         clickableViews.add(ctaView);
-
         // optional
         clickableViews.add(mediaView);
         clickableViews.add(titleTextView);
         clickableViews.add(descriptionTextView);
 
-        // 4) Call nativeAd.registerViewForInteraction().
-        nativeAd.registerViewForInteraction(adView, mediaView, clickableViews);
+        // 4) Setup relevant views and native ad.
+        nativeAdView.setMediaView(mediaView);
+        nativeAdView.setClickableViews(clickableViews);
+        nativeAdView.setNativeAd(nativeAd);
 
         return interstitialView;
     }
 
     private boolean closeInterstitialView() {
         if (interstitialView != null) {
-            nativeAd.unregisterView();
-            nativeAd = null;
             ((ViewGroup) interstitialView.getParent()).removeView(interstitialView);
             interstitialView = null;
             return true;
@@ -144,30 +147,6 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         if (!closeInterstitialView()) {
             super.onBackPressed();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (nativeAd != null) {
-            nativeAd.resume();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (nativeAd != null) {
-            nativeAd.pause();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (nativeAd != null) {
-            nativeAd.destroy();
         }
     }
 }
